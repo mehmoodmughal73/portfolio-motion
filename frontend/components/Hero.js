3d326239329dc23811d96a1ab8110ad85d587f91
Mehmood Ashraf <mughalmunda2024@gmail.com>
2025-08-25 17:44:53 +0500

    Update Hero.js

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section style={{ padding: "50px", textAlign: "center" }}>
-      <h1>🚀 Welcome to My Portfolio</h1>
+      <h1>Welcome to My Portfolio</h1>
       <p>I'm a Passionate DevOps Engineer</p>
     </section>
   );
